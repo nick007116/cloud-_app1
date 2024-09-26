abb6efb01a5b14374364ed5153ee0c8495354061
nick007116 <149318329+nick007116@users.noreply.github.com>
2024-09-26 14:45:09 +0530

    Add files via upload

--- average.py
+++ average.py
@@ -1,3 +1,12 @@
-numbers = list(map(float, input("Enter numbers separated by spaces: ").split()))
-average = sum(numbers) / len(numbers) if numbers else 0
-print(average)
+a=float(input("Enter a previous units:"))
+b=float(input("Enter a current units:"))
+c=a-b
+print(c)
+if c<=50:
+    print("congrats its free!")
+elif c>=50 and c<=100:
+    print("Your amount is:",c*2.60)
+elif c>=101 and c<=200:
+    print("Your amount is:",c*3.30)
+else:
+    print("Invalid")
